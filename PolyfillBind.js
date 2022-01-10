d7b0d99aa5ef5e240d6bb694f55ea626fd1645b4
Shivam Sagar <shivam.sagar@magicpin.in>
2022-01-11 01:43:27 +0530

    Pollyfill added for bind method

---
+++ PolyfillBind.js
@@ -0,0 +1,21 @@
+let name = {
+    firstName: "Shivam",
+    lastName: "Sagar"
+}
+
+let printMyName = function (city, country) {
+    console.log(this.firstName + " " + this.lastName, " " + city + " " + country)
+}
+
+let sayMyName = printMyName.bind(name, "Amritsar")
+sayMyName("India")
+
+Function.prototype.customBind = function(firstObj, ...args) {
+    let obj = this
+    return function(...args2) {
+        obj.apply(firstObj, [...args, ...args2])
+    }
+}
+
+let sayMyName2 = printMyName.customBind(name, "Amritsar")
+sayMyName2("India")
